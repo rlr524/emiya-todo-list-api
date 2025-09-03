1c71fd79c24bf5d4ce361253184feb9e3189d2f6
Rob Ranf <rob@emiyaconsulting.com>
2025-09-02 19:57:13 -0700

    adding mongo config and finishing item class

---
+++ src/main/java/com/emiyaconsulting/emiya_todo_list_api/config/MongoConfig.java
@@ -0,0 +1,9 @@
+package com.emiyaconsulting.emiya_todo_list_api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig {
+}
--- src/main/java/com/emiyaconsulting/emiya_todo_list_api/model/Item.java
+++ src/main/java/com/emiyaconsulting/emiya_todo_list_api/model/Item.java
@@ -1,11 +1,32 @@
 package com.emiyaconsulting.emiya_todo_list_api.model;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Date;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
 @Document(collation = "items")
 public class Item {
     @Id
     private String id;
     private String title;
+    private String description;
+    private Date due;
+    private boolean complete;
+    private String owner;
+    private boolean deleted;
+    @CreatedDate
+    private Date createdAt;
+    @LastModifiedDate
+    private Date updatedAt;
 }
