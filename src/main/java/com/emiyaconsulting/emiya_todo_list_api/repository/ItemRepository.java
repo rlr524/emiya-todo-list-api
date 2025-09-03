3113ff23585bd205d1e04a6c0ae68461fc6b187f
Rob Ranf <rob@emiyaconsulting.com>
2025-09-02 20:00:54 -0700

    adding item repository

---
+++ src/main/java/com/emiyaconsulting/emiya_todo_list_api/repository/ItemRepository.java
@@ -0,0 +1,9 @@
+package com.emiyaconsulting.emiya_todo_list_api.repository;
+
+import com.emiyaconsulting.emiya_todo_list_api.model.Item;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends MongoRepository<Item, String> {
+}
