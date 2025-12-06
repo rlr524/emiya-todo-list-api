de46d098c6854cef641853ee4804e032ffc375fd
Rob Ranf <rob@emiyaconsulting.com>
2025-12-05 17:50:55 -0800

    updating exception handler with controller advice annotation

--- src/main/java/com/emiyaconsulting/emiya_todo_list_api/exception/handler/ToDoListGlobalExceptionHandler.java
+++ src/main/java/com/emiyaconsulting/emiya_todo_list_api/exception/handler/ToDoListGlobalExceptionHandler.java
@@ -4,10 +4,12 @@ import com.emiyaconsulting.emiya_todo_list_api.exception.ItemNotFoundException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@ControllerAdvice
 public class ToDoListGlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(value = {ItemNotFoundException.class})
     public ResponseEntity<?> handleItemNotFound(ItemNotFoundException itemNotFoundException, WebRequest request) {
