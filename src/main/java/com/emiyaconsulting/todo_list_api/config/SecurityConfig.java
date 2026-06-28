863021500ca5953265d4fbda69c0ef2beb65bd3e
Rob Ranf <rob@emiyaconsulting.com>
2026-06-27 17:19:39 -0700

    adding user details service

--- src/main/java/com/emiyaconsulting/todo_list_api/config/SecurityConfig.java
+++ src/main/java/com/emiyaconsulting/todo_list_api/config/SecurityConfig.java
@@ -1,14 +1,42 @@
 package com.emiyaconsulting.todo_list_api.config;
 
+import com.emiyaconsulting.todo_list_api.security.JwtFilter;
+import com.emiyaconsulting.todo_list_api.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    private final JwtFilter jwtFilter;
+    private final UserService userService;
+
+    public SecurityConfig(JwtFilter jwtFilter, UserService userService) {
+        this.jwtFilter = jwtFilter;
+        this.userService = userService;
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+       
+    }
+    
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) 
+            throws Exception {
+        
     }
 }
--- src/main/java/com/emiyaconsulting/todo_list_api/repository/UserRepository.java
+++ src/main/java/com/emiyaconsulting/todo_list_api/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.emiyaconsulting.todo_list_api.repository;
 import com.emiyaconsulting.todo_list_api.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByUserName(String userName);
 }
