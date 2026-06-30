8f08af584b2fa6fc1a65e8f863af09e90f8e05bf
Rob Ranf <rob@emiyaconsulting.com>
2026-06-29 18:11:12 -0700

    finishing user details service impl

--- src/main/java/com/emiyaconsulting/todo_list_api/config/UserDetailsServiceImpl.java
+++ src/main/java/com/emiyaconsulting/todo_list_api/config/UserDetailsServiceImpl.java
@@ -1,10 +1,14 @@
 package com.emiyaconsulting.todo_list_api.config;
 
+import com.emiyaconsulting.todo_list_api.model.User;
 import com.emiyaconsulting.todo_list_api.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -16,6 +20,17 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> optionalUser = userRepository.findByUserName(username);
+        
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            return org.springframework.security.core.userdetails.User
+                    .withUsername(user.getUserName())
+                    .password(user.getPassword())
+                    .authorities(List.of())
+                    .build();
+        }
+        throw new UsernameNotFoundException("User name not found");
         
     }
 }
