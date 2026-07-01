78dda462603784507123b768ffa759846fc0af86
Rob Ranf <rob@emiyaconsulting.com>
2026-06-30 21:03:44 -0700

    finish filterChain method

--- src/main/java/com/emiyaconsulting/todo_list_api/config/SecurityConfig.java
+++ src/main/java/com/emiyaconsulting/todo_list_api/config/SecurityConfig.java
@@ -8,10 +8,11 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.FilterChainProxy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -31,12 +32,22 @@ public class SecurityConfig {
     
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-       
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session 
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        
+        return http.build();
     }
     
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) 
             throws Exception {
-        
+        return config.getAuthenticationManager();
     }
 }
