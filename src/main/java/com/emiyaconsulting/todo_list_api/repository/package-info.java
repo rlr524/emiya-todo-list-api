fd85d2d3cb09d246c113190c00434ff8f3d3dfaf
Rob Ranf <rob@emiyaconsulting.com>
2026-08-08 12:06:59 -0700

    adding some comments and package info files

---
+++ src/main/java/com/emiyaconsulting/todo_list_api/repository/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Provides interfaces for interaction with MongoDB extending the Spring Framework 
+ * MongoRepository interface.
+ * 
+ * Author: Rob Ranf
+ * rob@emiyaconsulting.com
+ * August 8, 2026
+ */
+
+package com.emiyaconsulting.todo_list_api.repository;
